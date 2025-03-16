c4bc0c7eaa9a59e7d51f77767bbcbb775e140d85
Stefan Anevski <anevskistefan11@gmail.com>
2025-03-17 00:21:02 +0100

    create ingredient type

---
+++ src/types/ingredient.ts
@@ -0,0 +1,36 @@
+export interface Ingredient {
+    id: string;
+    name: string;
+    amount: number;
+    unit: string;
+  }
+  
+  export interface Instruction {
+    id: string;
+    text: string;
+    description?: string; 
+    duration?: number; 
+    timerRequired?: boolean;
+  }
+  
+  export interface NutritionalInfo {
+    calories: number;
+    protein: number;
+    carbs: number;
+    fat: number;
+  }
+  
+  export interface Recipe {
+    id: string;
+    title: string;
+    image: string;
+    description: string;
+    readyInMinutes: number;
+    servings: number;
+    cuisine: string;
+    difficulty: string;
+    summary: string;
+    nutritionalInfo: NutritionalInfo;
+    ingredients: Ingredient[];
+    instructions: Instruction[];
+  } 
