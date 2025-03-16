26461f5ba89341a7624eee65d9184310a6a01901
Stefan Anevski <anevskistefan11@gmail.com>
2025-03-16 22:54:27 +0100

    add project dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.themealdb.com',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'vjfsascagdencbewveoz.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.in',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+        pathname: '**/storage/v1/object/public/**',
+      }
+    ],
+    domains: process.env.NODE_ENV === 'development' ? ['localhost', '*'] : [],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
