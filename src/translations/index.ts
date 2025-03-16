074b6843bff585a871a36fe0fe3840df948ae4b1
Martina <apostoloska21@gmail.com>
2025-03-16 23:47:32 +0100

    created index.ts for translations

---
+++ src/translations/index.ts
@@ -0,0 +1,22 @@
+import { en } from './en';
+import { es } from './es';
+import { fr } from './fr';
+import { de } from './de';
+
+export const translations = {
+  en,
+  es,
+  fr,
+  de,
+} as const;
+
+export type Language = keyof typeof translations;
+export type TranslationKeys = typeof en;
+
+export type TranslationPath = {
+  [K in keyof TranslationKeys]: {
+    [P in keyof TranslationKeys[K]]: `${K & string}.${P & string}`;
+  }[keyof TranslationKeys[K]];
+}[keyof TranslationKeys];
+
+export { en, es, fr, de }; 
